666892fba6a39dae87618f56e5b220c54e4c48f3
Domatyeva Angelina - BARL <gelyadoma@gmail.com>
2023-05-16 11:54:17 +0300

    Загрузка в бд списка продуктов - ok

--- pars_readme.py
+++ pars_readme.py
@@ -1,7 +1,7 @@
 from osgeo import gdal
 import os
-# from osgeo import ogr
-from pathlib import Path, PurePath
+from osgeo import ogr
+from pathlib import Path
 import re
 from pg_connect import pg_connect_insert
 
@@ -18,117 +18,63 @@ ATTRIBUTE_KEY = {
 }
 
 dict_content = {}
-imd_path = []  # список файлов метаданных
-tif_path = []  # список тифов
-
-path_folder = os.walk('D:/Dev/bd_satellite/products/')
 path_list = []
 
 
-def path_walk(path_folder):
-    # for path_folder, dirs, filenames in path_folder:
-    #     pattern = re.compile(r'^[0-9]+.\d{2}.[A-Z]+\d{3}.\D{3}') 
-    #     matches = [subdir for subdir in dirs if pattern.match(subdir)]
-    #     for path in matches:
-    #         print(path)
-    # return 
+def path_walk():
     rootdir = r'D:\Dev\bd_satellite\products'
     for path in Path(rootdir).iterdir():
-        # print(path)
         path_list.append(os.path.join(path))
     for p in path_list:
-        # print(p) # корневые папки продуктов
         for path in os.listdir(p):  # для каждой подпапки в папке продукта
             pattern = re.compile(r'^[0-9]+.\d{2}.[A-Z]+\d{3}.\D{3}')
-            if pattern.match(path):  # сравнить с шаблоном названия папки с ихображением
+            if pattern.match(path):  # сравнить с шаблоном названия папки с изображением
                 path_img = os.path.join(p, path)  # сгенерить путь папка продукта + папка с изображением
-                # print(path_img)
-                wv_pars(path_img)
-        # print(tif_pars(tif_path))
-            # pg_connect_insert(dict_content)
+                # pg_connect_insert(wv_pars(path_img))
+                print(wv_pars(path_img))
     return
 
 
 def wv_pars(path_img):
     files = os.listdir(path_img)
-    # print(files)
     for f in files:
         file = os.path.join(f)
-        # print(file)
         match_imd = re.search(r'^.+IMD', file)
-        match_tif = re.search(r'.+TIF$', file)
+        match_tif = re.search(r'^.+TIL$', file)
         if match_imd:
             file_path = os.path.join(path_img, file)
-            # imd_path.append(file_path)
-            print(imd_pars(file_path))
-            # print(file_path)
+            imd_pars(file_path)
         elif match_tif:
-            tif_path.append(file)
-    print(imd_path)
-    return 
-    # print(imd_path)
-    # return dict_content
-    # print(tif_path)
-    
-    # print(shp_path)
-# meanCollectedGSD
-
-
-def imd_pars(file_path):
-    # # print(imd_path)
-    # for p in imd_path:
-    #     # print(p)
-        with open(file_path) as f:
-            content = list(filter(None, f.read().strip().split(';\n')))
-            # print(content)
-            for el in content:
-                params = el.strip('\t').split()
-            # print(params)
-                if len(params) > 3:
-                    if params[3] in ATTRIBUTE_KEY.keys():
-                        value_param = params[5]
-                        key_dict = ATTRIBUTE_KEY[params[3]]
-                        dict_content[key_dict] = value_param.strip('"')
-                else:
-                    if params[0] in ATTRIBUTE_KEY.keys():
-                        value_param = params[2]
-                        key_dict = ATTRIBUTE_KEY[params[0]]
-                        dict_content[key_dict] = value_param.strip('"')
-        return dict_content
-
-
-def tif_pars(tif_path):
-    for p in tif_path:
-        gtif = gdal.Open(p)
-        band_numbers = gtif.RasterCount  # band numbers
-        dict_content['band_numbers'] = band_numbers
+            file_path = os.path.join(path_img, file)
+            tif_pars(file_path)
     return dict_content
 
 
+def imd_pars(file_path):
+    with open(file_path) as f:
+        content = list(filter(None, f.read().strip().split(';\n')))
+        for el in content:
+            params = el.strip('\t').split()
+            if len(params) > 3:
+                if params[3] in ATTRIBUTE_KEY.keys():
+                    value_param = params[5]
+                    key_dict = ATTRIBUTE_KEY[params[3]]
+                    dict_content[key_dict] = value_param.strip('"')
+            else:
+                if params[0] in ATTRIBUTE_KEY.keys():
+                    value_param = params[2]
+                    key_dict = ATTRIBUTE_KEY[params[0]]
+                    dict_content[key_dict] = value_param.strip('"')
+    return dict_content
 
-# rootdir = '../bd_satellite/products/'
-# path_list = []
 
-# for path in Path(rootdir).iterdir():
-#     if path.is_dir():
-#         path_list.append(os.path.join(path))
-#         for p in path_list:
-#             wv_pars(p)
-# print(path_list)
-# # def shp_pars(shp_path):
-# #     layerList = []
-# #     for p in shp_path:
-# #         gshp = ogr.Open(p)
-# #         layer = gshp.GetName()
-# #         print(layer)
-# #         layerList.append(layer)
-# #     dict_content['mask'] = layerList
-# #     return dict_content
+def tif_pars(tif_path):
+    gtif = gdal.Open(tif_path)
+    # print(gtif)
+    band_numbers = gtif.RasterCount  # band numbers
+    dict_content['band_numbers'] = band_numbers
+    # print(dict_content)
+    return dict_content
 
-# def insert_pgdb():
-path_walk(path_folder)
-# wv_pars(path_list)
-# imd_pars(imd_path)
-# print(tif_pars(tif_path))
 
-# print(shp_pars(shp_path))
+path_walk()
--- pg_connect.py
+++ pg_connect.py
@@ -1,18 +1,18 @@
 import psycopg2
 from psycopg2 import Error
-from pars_readme import dict_content
+# from pars_readme import dict_content
 # from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 sql_create_table = "CREATE TABLE IMAGE (ID SERIAL PRIMARY KEY,\
-          NAME_PRODUCT TEXT,\
-          SATELLITE_NAME TEXT,\
-          PIXEL_RESOLUTION FLOAT,\
-          BAND_INFO TEXT,\
-          BAND_NUMBERS SMALLINT,\
-          PROCESSING_LEVEL TEXT,\
-          FORMAT SMALLINT,\
-          DATE TIMESTAMPTZ,\
-          CLOUD_COVER FLOAT);"  # запрос на создание бд через криейт
+    NAME_PRODUCT TEXT,\
+    SATELLITE_NAME TEXT,\
+    PIXEL_RESOLUTION FLOAT,\
+    BAND_INFO TEXT,\
+    BAND_NUMBERS SMALLINT,\
+    PROCESSING_LEVEL TEXT,\
+    FORMAT SMALLINT,\
+    DATE TIMESTAMPTZ,\
+    CLOUD_COVER FLOAT);"  # запрос на создание бд через криейт
 
 sql_insert_data = "INSERT INTO image (name_product, satellite_name,\
     pixel_resolution,\
@@ -24,36 +24,41 @@ sql_insert_data = "INSERT INTO image (name_product, satellite_name,\
         VALUES (%(name_product)s, %(satellite_name)s, %(pixel_resolution)s,\
         %(band_info)s,\
         %(processing_level)s,\
-        %(format)s,%(band_numbers)s,\
+        %(band_numbers)s,%(format)s,\
         %(date)s, %(cloud_cover)s);"
 
-try:
-    connection = psycopg2.connect(
-        port='5433',
-        dbname='test_db',
-        user='postgres',
-        password='postgres'
-    )
-    # connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    print('Вы подключились к базе')
-    cursor = connection.cursor()
-    # print('Информация о сервере PostgreSQL')
-    # print(connection.get_dsn_parameters(), '\n')
-    cursor.execute(sql_insert_data, dict_content)
-    connection.commit()  # обязательно при выполнении запросов в бд
-    # record = cursor.fetchone()
-    print('Запись добавлена')
-except (Exception, Error) as error:
-    print('Какая то фигня с подключением блин,', error)
-    # connectioin.rollback() - если после запросов что-то не то - можно откатиться до предыдущего состояния базы
-    # psycopg2.DatabaseError -  чтобы поймать все ошибки от одного оператора
-finally:
-    if connection:
-        cursor.close()
-        connection.close()
-        print('Соединение закрыто')
+
+def pg_connect_insert(dict_content):
+    try:
+        connection = psycopg2.connect(
+            port='5433',
+            dbname='test_db',
+            user='postgres',
+            password='postgres'
+        )
+        # connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        print('Вы подключились к базе')
+        cursor = connection.cursor()
+        # print('Информация о сервере PostgreSQL')
+        # print(connection.get_dsn_parameters(), '\n')
+        cursor.execute(sql_insert_data, dict_content)
+        connection.commit()  # обязательно при выполнении запросов в бд
+        # record = cursor.fetchone()
+        print('Запись добавлена')
+    except (Exception, Error) as error:
+        print('Какая то фигня с подключением блин,', error)
+        # connectioin.rollback() - если после запросов что-то не то - можно откатиться до предыдущего состояния базы
+        # psycopg2.DatabaseError -  чтобы поймать все ошибки от одного оператора
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print('Соединение закрыто')
 
 # with - для создания транзакции внутри определенного блока в коде
 # позволяет не писать явно коммит и ролбэк
 
 # ИНСЕРТ ДИНАМИЧЕСКИ
+
+
+# pg_connect_insert()
